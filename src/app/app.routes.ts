3501473cfc1f73827dc6903f158d35facd2568ba
Adolf0M <adolfo@tecnofy.org>
2024-05-16 22:08:30 -0700

    Se finaliza front de pagina register y login #4

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
@@ -12,4 +13,22 @@ export const routes: Routes = [
     loadComponent: () => 
       import('./pages/home/home.component').then((m) => m.HomeComponent),
   },
+  {
+    path: 'login',
+    title: 'Iniciar Sesión',
+    loadComponent: () => 
+      import('./pages/login/login.component').then((m) => m.LoginComponent),
+  },
+  {
+    path: 'register',
+    title: 'Registrate',
+    loadComponent: () =>
+      import('./pages/register/register.component').then((m) => m.RegisterComponent),
+  }
 ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
