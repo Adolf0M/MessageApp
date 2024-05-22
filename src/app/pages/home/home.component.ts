8965408fd4ff5b16c6802287f131ea4b1439c851
Adolf0M <adolfo@tecnofy.org>
2024-05-21 21:48:42 -0700

    Se realizo pantalla para conversacion, pero falta ajustar altura para que no desborde la pantalla

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { ChatComponent } from "../../components/home/chat/chat.component";
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+    selector: 'app-home',
+    standalone: true,
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.scss',
+    imports: [ChatComponent]
 })
 export class HomeComponent {
 
