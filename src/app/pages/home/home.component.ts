6081c3b82a3f09117263b8e1c312212b3c418c1b
Adolf0M <adolfo@tecnofy.org>
2024-05-26 21:54:54 -0700

    Se finalizo con reestructuracion de la pagina completa, añadiendo el navbar solo en la parte donde se mostrarán las conversaciones recientes

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { ChatComponent } from "../../components/home/chat/chat.component";
+import { NavbarComponent } from "../../components/navbar/navbar.component";
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss',
-    imports: [ChatComponent]
+    imports: [ChatComponent, NavbarComponent]
 })
 export class HomeComponent {
 
