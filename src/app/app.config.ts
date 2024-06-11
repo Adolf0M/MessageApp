a4085d39bcf09d3254ea77a09f9d15d4d76c34a2
unknown <merchantnevarez.es@gmail.com>
2024-06-10 19:10:21 -0700

    Se modificaron archivos y se preparo ng build para subir al host-deploy

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,21 @@
 import { ApplicationConfig, provideZoneChangeDetection } from "@angular/core";
-import { provideRouter } from "@angular/router";
+import { provideRouter, withHashLocation, withInMemoryScrolling } from "@angular/router";
 
 import { routes } from "./app.routes";
 import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
 import { initializeApp, provideFirebaseApp } from "@angular/fire/app";
 import { getAuth, provideAuth } from "@angular/fire/auth";
+import { provideHttpClient } from "@angular/common/http";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(
+      routes,
+      withHashLocation(),
+      withInMemoryScrolling({ scrollPositionRestoration: 'top' })
+    ),
+    provideHttpClient(),
     provideAnimationsAsync(),
     provideFirebaseApp(() =>
       initializeApp({
