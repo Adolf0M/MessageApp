873b21a5a76824b14f83d0363b15deccd0947e86
erickmerchant05 <merchantnevarez.es@gmail.com>
2024-06-11 19:10:35 -0700

    se arreglaron rutas con hash #24

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from "@angular/core";
-import { provideRouter } from "@angular/router";
+import { provideRouter, withHashLocation, withInMemoryScrolling } from "@angular/router";
 
 import { routes } from "./app.routes";
 import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
@@ -9,7 +9,11 @@ import { getAuth, provideAuth } from "@angular/fire/auth";
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(
+      routes,
+      withHashLocation(),
+      withInMemoryScrolling({ scrollPositionRestoration: 'top' })
+    ),
     provideAnimationsAsync(),
     provideFirebaseApp(() =>
       initializeApp({
